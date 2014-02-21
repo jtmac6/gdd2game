fc78e97ccaa09c5cd7e15f0c8553d012445e888f
Ryan Rule-Hoffman <ry60003333@mac.com>
2014-02-21 16:04:20 -0500

    Added blank fullscreen canvas.

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+window.onload = init;
+
+function init() {
+	// Grab our canvas.
+	var canvas = document.getElementById('canvas');
+	
+	// Make the canvas fullscreen
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	
+	// We'll need to listen for when the page is resized and adjust accordingly.
+}
