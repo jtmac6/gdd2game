a71eac1607e4634e741c0d30c1e60d66ca777693
Ryan Rule-Hoffman <ry60003333@mac.com>
2014-02-25 10:28:40 -0500

    Added basic collision detection

--- src/js/Scene.js
+++ src/js/Scene.js
@@ -38,8 +38,32 @@ var Scene = function(sceneNum){
 	}
 	//checks collisions, also resolves them
 	this.checkCollisions = function(){
-
+		for (var i = this.gameEntities.length - 1; i >= 0; i--) {
+			var nextEntity = gameEntities[i];
+			// Check if the entity intersects with the player
+			if (intersectsPlayer(entity))
+			{
+				// Handle collision!
+				
+				// If the player is coming up from the bottom, 
+				// Set a variable to ignore the collision until they are no 
+				// longer colliding with the entity
+				
+			}
+		};
 	}
+	
+	// Check if an entity intersects the player.
+	this.intersectsPlayer = function(entity) {
+		var noOverlap = (
+			entity.x > player.x+player.width ||
+			player.x > entity.x+entity.width ||
+			entity.y > player.y+player.height ||
+			player.y > entity.y+entity.height
+			);
+		return !noOverlap;
+	};
+	
 	//updates the game to the next state
 	this.update = function(ctx){
 		this.draw(ctx);
